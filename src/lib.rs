fd6cc5769d9936bb93bd39f7e2255892b5a3f061
shota-tsuji <shotatuzi@gmail.com>
2021-05-21 18:47:45 +0900

    feat: 小文字化された文字列を返す機能を追加

--- src/lib.rs
+++ src/lib.rs
@@ -2,5 +2,26 @@ pub trait StringTranslate {
     /// 文字列を受け取って小文字にして返すメソッド
     /// @param word 操作対象の文字列
     /// @return 小文字化された文字列
-    fn to_lower<'a>(word: &'a str) -> &'a str;
+    fn to_lower(word: &str) -> String;
+}
+
+struct StringTranslator {
+}
+
+impl StringTranslate for StringTranslator {
+    fn to_lower(word: &str) -> String {
+        word.to_lowercase()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn given_capital_return_lower_characters() {
+        let word = "aBc";
+        assert_eq!("abc", StringTranslator::to_lower(&word));
+        let word = "DEF";
+        assert_eq!("def", StringTranslator::to_lower(&word));
+    }
 }
