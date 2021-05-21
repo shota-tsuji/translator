4082b388a28d5e3efff1312b4318edf59c5978b8
shota-tsuji <shotatuzi@gmail.com>
2021-05-22 04:15:04 +0900

    feat: 文字列の検証用メソッドの正常系を実装

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,17 @@
+use regex::Regex;
+
 pub trait StringTranslate {
     /// 文字列を受け取って小文字にして返すメソッド
     /// @param word 操作対象の文字列
     /// @return 小文字化された文字列
     fn to_lower(word: &str) -> String;
+}
+
+pub trait StringValidate {
+    /// 文字列を受け取ってアルファベットのみであるかどうかを返すメソッド
+    /// @param word 確認対象の文字列
+    /// @return アルファベットのみの文字列であるかどうか
+    fn validate(&self, word: &str) -> bool;
 }
 
 struct StringTranslator {
@@ -11,6 +20,24 @@ struct StringTranslator {
 impl StringTranslate for StringTranslator {
     fn to_lower(word: &str) -> String {
         word.to_lowercase()
+    }
+}
+
+struct StringValidator {
+    re: regex::Regex,
+}
+
+impl StringValidator {
+    fn new() -> StringValidator {
+        StringValidator {
+            re: Regex::new(r"[[:alpha:]]+").unwrap(),
+        }
+    }
+}
+
+impl StringValidate for StringValidator {
+    fn validate(&self, word: &str) -> bool {
+        self.re.is_match(word)
     }
 }
 
@@ -23,5 +50,12 @@ mod tests {
         assert_eq!("abc", StringTranslator::to_lower(&word));
         let word = "DEF";
         assert_eq!("def", StringTranslator::to_lower(&word));
+    }
+
+    #[test]
+    fn given_alphabets_return_true() {
+        let word = "validString";
+        let string_validator: StringValidator = StringValidator::new();
+        assert!(string_validator.validate(&word));
     }
 }
